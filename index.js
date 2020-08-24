d8294bb70684a8974b1c510756a07ccfe43c1f06
Antonio <antonioluciofb@hotmail.com>
2020-08-24 15:17:36 -0300

    .

--- index.js
+++ index.js
@@ -53,23 +53,30 @@ const puppeteer = require('puppeteer');
         const page = await browser.newPage()
         await page.goto('https://www.lance24h.com.br')
         await page.click('li.Li2 a')
+        await page.waitFor(1000)
         await page.type('input#Log_Email', 'antonioluciofb', {delay:300})
+        await page.waitFor(1000)
         await page.type('input#Log_Senha', 'aleatorio22A', {delay:300})
+        await page.waitFor(1000)
         await page.click('div#FFormLogin_B button')
         await page.waitFor(2000)
-        await page.goto('https://www.lance24h.com.br/Detalhes.php?Titulo=IMac-com-Intel-Core-i5-Dual-Core-8GB-1TB-21.5-Prata-MMQA2BZA-_-Apple&Codigo=43030')
+        await page.goto('https://www.lance24h.com.br/Detalhes.php?Titulo=Pacote-50-Lances&Codigo=43042')
         await page.waitFor(2000)
-
         const timer = await page.evaluate ( async () =>{
+          
+          setInterval(function(){ 
             
-          const contador = document.getElementById("L_ContDown_1_43030")
-          const contador2 = document.getElementById("L_ContDown_2_43030")
+          const contador = document.getElementById("L_ContDown_1_43042")
+          const contador2 = document.getElementById("L_ContDown_2_43042")
           console.log(contador.textContent)
           console.log(contador2.textContent)
 
-          if (contador == 0 && contador2 == 8) {
-              await page.click('div#L_BotaoA_43030 a')
-          }
+          // if (contador == 0 && contador2 == 8) {
+          //     await page.click('div#L_BotaoA_43042 a')
+          // } 
+        
+              }, 1000);
+         
         })
       })()
     } catch (err) {
